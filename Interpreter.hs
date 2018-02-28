065460a55b28df3db23d89dc497b9f03807cf087
Oliver Martin <olivermartin410@yahoo.co.uk>
2018-02-28 20:30:08 +0000

    Updated files for billygit add *

--- Interpreter.hs
+++ Interpreter.hs
@@ -1,4 +1,31 @@
 import Tokens
 import Grammar
-interpret :: Prog -> String
-interpret (ProgLink p1 p2) = (interpret p1) ++ (interpret p2)
+
+type Relation = (String,[VarItem],[[String]])
+
+eval :: Prog -> String
+eval (ProgLink p1 p2) = (eval p1) ++ (eval p2)
+eval (Query svl cl) = query svl cl
+
+query :: StrictVarList -> ConstraintList -> String
+query (StrictVarListSingleton var) cl = 
+
+parseCSV :: [[String]] -> String -> [VarItem] -> Relation
+parseCSV xss rel cols =
+
+readCSV :: String -> IO [[String]]
+readCSV relName = --TODO
+
+
+
+
+
+
+
+convertVarList :: VarList -> [VarItem]
+convertVarList (VarListSingleton vi) = [vi]
+convertVarList (VarListLink vi vl) = [vi] ++ convertVarList vl
+
+convertStrictVarList :: StrictVarList -> [String]
+convertStrictVarList (StrictVarListSingleton vi) = [vi]
+convertStrictVarList (StrictVarListLink vi vl) = [vi] ++ convertStrictVarList vl
