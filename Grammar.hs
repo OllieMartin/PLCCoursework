fa9c8436610f6cad75429b30e6cc2f568196b0b8
Billy Bradley <billy.jack.bradley@gmail.com>
2018-03-04 15:28:36 +0000

    made grammar nice

--- Grammar.hs
+++ Grammar.hs
@@ -1,10 +1,12 @@
 {-# OPTIONS_GHC -w #-}
 module Grammar where 
 import Tokens
+import qualified Data.Array as Happy_Data_Array
+import qualified Data.Bits as Bits
 import Control.Applicative(Applicative(..))
 import Control.Monad (ap)
 
--- parser produced by Happy Version 1.19.5
+-- parser produced by Happy Version 1.19.9
 
 data HappyAbsSyn t4 t5 t6 t7 t8 t9 t10
 	= HappyTerminal (Token)
@@ -17,60 +19,77 @@ data HappyAbsSyn t4 t5 t6 t7 t8 t9 t10
 	| HappyAbsSyn9 t9
 	| HappyAbsSyn10 t10
 
+happyExpList :: Happy_Data_Array.Array Int Int
+happyExpList = Happy_Data_Array.listArray (0,56) ([2048,0,8,32768,0,512,0,0,128,3072,0,8,3072,0,256,0,4,4128,4096,0,0,0,1,8,2048,0,2056,0,64,12,0,0,0,0,32,512,16384,0,512,0,0,0,0,0,0,2048,8,0,0,64,32,2048,8,0,0,0,64,0,0,0
+	])
+
+{-# NOINLINE happyExpListPerState #-}
+happyExpListPerState st =
+    token_strs_expected
+  where token_strs = ["error","%dummy","%start_parseCalc","Prog","VarList","StrictVarList","VarItem","ConstraintList","Constraint","NumList","rel","var","'='","'('","')'","'{'","'}'","','","';'","'_'","'['","']'","num","%eof"]
+        bit_start = st * 24
+        bit_end = (st + 1) * 24
+        read_bit = readArrayBit happyExpList
+        bits = map read_bit [bit_start..bit_end - 1]
+        bits_indexed = zip bits [0..23]
+        token_strs_expected = concatMap f bits_indexed
+        f (False, _) = []
+        f (True, nr) = [token_strs !! nr]
+
 action_0 (12) = happyShift action_3
 action_0 (4) = happyGoto action_4
 action_0 (6) = happyGoto action_5
-action_0 _ = happyFail
+action_0 _ = happyFail (happyExpListPerState 0)
 
 action_1 (12) = happyShift action_3
 action_1 (6) = happyGoto action_2
-action_1 _ = happyFail
+action_1 _ = happyFail (happyExpListPerState 1)
 
 action_2 (16) = happyShift action_8
-action_2 _ = happyFail
+action_2 _ = happyFail (happyExpListPerState 2)
 
 action_3 (18) = happyShift action_7
 action_3 _ = happyReduce_5
 
 action_4 (24) = happyAccept
-action_4 _ = happyFail
+action_4 _ = happyFail (happyExpListPerState 4)
 
 action_5 (16) = happyShift action_6
-action_5 _ = happyFail
+action_5 _ = happyFail (happyExpListPerState 5)
 
 action_6 (11) = happyShift action_11
 action_6 (12) = happyShift action_12
 action_6 (8) = happyGoto action_14
 action_6 (9) = happyGoto action_10
-action_6 _ = happyFail
+action_6 _ = happyFail (happyExpListPerState 6)
 
 action_7 (12) = happyShift action_3
 action_7 (6) = happyGoto action_13
-action_7 _ = happyFail
+action_7 _ = happyFail (happyExpListPerState 7)
 
 action_8 (11) = happyShift action_11
 action_8 (12) = happyShift action_12
 action_8 (8) = happyGoto action_9
 action_8 (9) = happyGoto action_10
-action_8 _ = happyFail
+action_8 _ = happyFail (happyExpListPerState 8)
 
 action_9 (17) = happyShift action_20
-action_9 _ = happyFail
+action_9 _ = happyFail (happyExpListPerState 9)
 
 action_10 (19) = happyShift action_19
-action_10 _ = happyFail
+action_10 _ = happyFail (happyExpListPerState 10)
 
 action_11 (14) = happyShift action_17
 action_11 (21) = happyShift action_18
-action_11 _ = happyFail
+action_11 _ = happyFail (happyExpListPerState 11)
 
 action_12 (13) = happyShift action_16
-action_12 _ = happyFail
+action_12 _ = happyFail (happyExpListPerState 12)
 
 action_13 _ = happyReduce_6
 
 action_14 (17) = happyShift action_15
-action_14 _ = happyFail
+action_14 _ = happyFail (happyExpListPerState 14)
 
 action_15 (12) = happyShift action_3
 action_15 (4) = happyGoto action_29
@@ -78,17 +97,17 @@ action_15 (6) = happyGoto action_5
 action_15 _ = happyReduce_1
 
 action_16 (12) = happyShift action_28
-action_16 _ = happyFail
+action_16 _ = happyFail (happyExpListPerState 16)
 
 action_17 (12) = happyShift action_26
 action_17 (20) = happyShift action_27
 action_17 (5) = happyGoto action_24
 action_17 (7) = happyGoto action_25
-action_17 _ = happyFail
+action_17 _ = happyFail (happyExpListPerState 17)
 
 action_18 (23) = happyShift action_23
 action_18 (10) = happyGoto action_22
-action_18 _ = happyFail
+action_18 _ = happyFail (happyExpListPerState 18)
 
 action_19 (11) = happyShift action_11
 action_19 (12) = happyShift action_12
@@ -96,18 +115,18 @@ action_19 (8) = happyGoto action_21
 action_19 (9) = happyGoto action_10
 action_19 _ = happyReduce_9
 
-action_20 _ = happyFail
+action_20 _ = happyFail (happyExpListPerState 20)
 
 action_21 _ = happyReduce_10
 
 action_22 (22) = happyShift action_33
-action_22 _ = happyFail
+action_22 _ = happyFail (happyExpListPerState 22)
 
 action_23 (18) = happyShift action_32
 action_23 _ = happyReduce_14
 
 action_24 (15) = happyShift action_31
-action_24 _ = happyFail
+action_24 _ = happyFail (happyExpListPerState 24)
 
 action_25 (18) = happyShift action_30
 action_25 _ = happyReduce_3
@@ -124,29 +143,29 @@ action_30 (12) = happyShift action_26
 action_30 (20) = happyShift action_27
 action_30 (5) = happyGoto action_36
 action_30 (7) = happyGoto action_25
-action_30 _ = happyFail
+action_30 _ = happyFail (happyExpListPerState 30)
 
 action_31 _ = happyReduce_11
 
 action_32 (23) = happyShift action_23
 action_32 (10) = happyGoto action_35
-action_32 _ = happyFail
+action_32 _ = happyFail (happyExpListPerState 32)
 
 action_33 (14) = happyShift action_34
-action_33 _ = happyFail
+action_33 _ = happyFail (happyExpListPerState 33)
 
 action_34 (12) = happyShift action_26
 action_34 (20) = happyShift action_27
 action_34 (5) = happyGoto action_37
 action_34 (7) = happyGoto action_25
-action_34 _ = happyFail
+action_34 _ = happyFail (happyExpListPerState 34)
 
 action_35 _ = happyReduce_15
 
 action_36 _ = happyReduce_4
 
 action_37 (15) = happyShift action_38
-action_37 _ = happyFail
+action_37 _ = happyFail (happyExpListPerState 37)
 
 action_38 _ = happyReduce_12
 
@@ -174,7 +193,7 @@ happyReduction_2 ((HappyAbsSyn4  happy_var_5) `HappyStk`
 happyReduce_3 = happySpecReduce_1  5 happyReduction_3
 happyReduction_3 (HappyAbsSyn7  happy_var_1)
 	 =  HappyAbsSyn5
-		 (VarListSingleton happy_var_1
+		 ([happy_var_1]
 	)
 happyReduction_3 _  = notHappyAtAll 
 
@@ -183,14 +202,14 @@ happyReduction_4 (HappyAbsSyn5  happy_var_3)
 	_
 	(HappyAbsSyn7  happy_var_1)
 	 =  HappyAbsSyn5
-		 (VarListLink happy_var_1 happy_var_3
+		 (happy_var_1 : happy_var_3
 	)
 happyReduction_4 _ _ _  = notHappyAtAll 
 
 happyReduce_5 = happySpecReduce_1  6 happyReduction_5
 happyReduction_5 (HappyTerminal (TokenVar _ happy_var_1))
 	 =  HappyAbsSyn6
-		 (StrictVarListSingleton happy_var_1
+		 ([happy_var_1]
 	)
 happyReduction_5 _  = notHappyAtAll 
 
@@ -199,7 +218,7 @@ happyReduction_6 (HappyAbsSyn6  happy_var_3)
 	_
 	(HappyTerminal (TokenVar _ happy_var_1))
 	 =  HappyAbsSyn6
-		 (StrictVarListLink happy_var_1 happy_var_3
+		 (happy_var_1 : happy_var_3
 	)
 happyReduction_6 _ _ _  = notHappyAtAll 
 
@@ -220,7 +239,7 @@ happyReduce_9 = happySpecReduce_2  8 happyReduction_9
 happyReduction_9 _
 	(HappyAbsSyn9  happy_var_1)
 	 =  HappyAbsSyn8
-		 (ConstraintListSingleton happy_var_1
+		 ([happy_var_1]
 	)
 happyReduction_9 _ _  = notHappyAtAll 
 
@@ -229,7 +248,7 @@ happyReduction_10 (HappyAbsSyn8  happy_var_3)
 	_
 	(HappyAbsSyn9  happy_var_1)
 	 =  HappyAbsSyn8
-		 (ConstraintListLink happy_var_1 happy_var_3
+		 (happy_var_1 : happy_var_3
 	)
 happyReduction_10 _ _ _  = notHappyAtAll 
 
@@ -268,7 +287,7 @@ happyReduction_13 _ _ _  = notHappyAtAll
 happyReduce_14 = happySpecReduce_1  10 happyReduction_14
 happyReduction_14 (HappyTerminal (TokenNum _ happy_var_1))
 	 =  HappyAbsSyn10
-		 (NumListSingleton happy_var_1
+		 ([happy_var_1]
 	)
 happyReduction_14 _  = notHappyAtAll 
 
@@ -277,7 +296,7 @@ happyReduction_15 (HappyAbsSyn10  happy_var_3)
 	_
 	(HappyTerminal (TokenNum _ happy_var_1))
 	 =  HappyAbsSyn10
-		 (NumListLink happy_var_1 happy_var_3
+		 (happy_var_1 : happy_var_3
 	)
 happyReduction_15 _ _ _  = notHappyAtAll 
 
@@ -300,11 +319,11 @@ happyNewToken action sts stk (tk:tks) =
 	TokenOpenSqr _ -> cont 21;
 	TokenCloseSqr _ -> cont 22;
 	TokenNum _ happy_dollar_dollar -> cont 23;
-	_ -> happyError' (tk:tks)
+	_ -> happyError' ((tk:tks), [])
 	}
 
-happyError_ 24 tk tks = happyError' tks
-happyError_ _ tk tks = happyError' (tk:tks)
+happyError_ explist 24 tk tks = happyError' (tks, explist)
+happyError_ explist _ tk tks = happyError' ((tk:tks), explist)
 
 newtype HappyIdentity a = HappyIdentity a
 happyIdentity = HappyIdentity
@@ -314,10 +333,10 @@ instance Functor HappyIdentity where
     fmap f (HappyIdentity a) = HappyIdentity (f a)
 
 instance Applicative HappyIdentity where
-    pure  = return
+    pure  = HappyIdentity
     (<*>) = ap
 instance Monad HappyIdentity where
-    return = HappyIdentity
+    return = pure
     (HappyIdentity p) >>= q = q p
 
 happyThen :: () => HappyIdentity a -> (a -> HappyIdentity b) -> HappyIdentity b
@@ -327,11 +346,10 @@ happyReturn = (return)
 happyThen1 m k tks = (>>=) m (\a -> k a tks)
 happyReturn1 :: () => a -> b -> HappyIdentity a
 happyReturn1 = \a tks -> (return) a
-happyError' :: () => [(Token)] -> HappyIdentity a
-happyError' = HappyIdentity . parseError
-
+happyError' :: () => ([(Token)], [String]) -> HappyIdentity a
+happyError' = HappyIdentity . (\(tokens, _) -> parseError tokens)
 parseCalc tks = happyRunIdentity happySomeParser where
-  happySomeParser = happyThen (happyParse action_0 tks) (\x -> case x of {HappyAbsSyn4 z -> happyReturn z; _other -> notHappyAtAll })
+ happySomeParser = happyThen (happyParse action_0 tks) (\x -> case x of {HappyAbsSyn4 z -> happyReturn z; _other -> notHappyAtAll })
 
 happySeq = happyDontSeq
 
@@ -339,40 +357,22 @@ happySeq = happyDontSeq
 parseError :: [Token] -> a
 parseError _ = error "Parse error"
   
-data Prog = Query StrictVarList ConstraintList
+data Prog = Query [String] [Constraint]
          | ProgLink Prog Prog
          deriving Show
-
-data VarList = VarListSingleton VarItem
-  | VarListLink VarItem VarList
-  deriving Show
-  
-data StrictVarList = StrictVarListSingleton String
-  | StrictVarListLink String StrictVarList
-  deriving Show
 
 data VarItem = VarItemVar String
   | VarItemBlank
   deriving Show
 
-data ConstraintList = ConstraintListSingleton Constraint
-  | ConstraintListLink Constraint ConstraintList
-  deriving Show
-
-data Constraint = ConstraintRel String VarList
-  | ConstraintRelEnhanced String NumList VarList
+data Constraint = ConstraintRel String [VarItem]
+  | ConstraintRelEnhanced String [Int] [VarItem]
   | ConstraintEq String String
   deriving Show
-
-data NumList = NumListSingleton Int
-  | NumListLink Int NumList
-  deriving Show
-{-# LINE 1 "templates\GenericTemplate.hs" #-}
-{-# LINE 1 "templates\\GenericTemplate.hs" #-}
+{-# LINE 1 "templates/GenericTemplate.hs" #-}
+{-# LINE 1 "templates/GenericTemplate.hs" #-}
 {-# LINE 1 "<built-in>" #-}
 {-# LINE 1 "<command-line>" #-}
-{-# LINE 8 "<command-line>" #-}
-{-# LINE 1 "G:\\GitHub\\haskell-platform\\build\\ghc-bindist\\local\\lib/include\\ghcversion.h" #-}
 
 
 
@@ -380,7 +380,9 @@ data NumList = NumListSingleton Int
 
 
 
+# 1 "/usr/include/stdc-predef.h" 1 3 4
 
+# 17 "/usr/include/stdc-predef.h" 3 4
 
 
 
@@ -388,8 +390,6 @@ data NumList = NumListSingleton Int
 
 
 
-{-# LINE 8 "<command-line>" #-}
-{-# LINE 1 "C:\\Users\\randy\\AppData\\Local\\Temp\\ghc8056_0\\ghc_2.h" #-}
 
 
 
@@ -429,6 +429,8 @@ data NumList = NumListSingleton Int
 
 
 
+{-# LINE 7 "<command-line>" #-}
+{-# LINE 1 "/usr/lib/ghc-8.2.2/include/ghcversion.h" #-}
 
 
 
@@ -444,6 +446,8 @@ data NumList = NumListSingleton Int
 
 
 
+{-# LINE 7 "<command-line>" #-}
+{-# LINE 1 "/tmp/ghc549_0/ghc_2.h" #-}
 
 
 
@@ -546,13 +550,76 @@ data NumList = NumListSingleton Int
 
 
 
-{-# LINE 8 "<command-line>" #-}
-{-# LINE 1 "templates\\GenericTemplate.hs" #-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+{-# LINE 7 "<command-line>" #-}
+{-# LINE 1 "templates/GenericTemplate.hs" #-}
 -- Id: GenericTemplate.hs,v 1.26 2005/01/14 14:47:22 simonmar Exp 
 
-{-# LINE 13 "templates\\GenericTemplate.hs" #-}
-
-{-# LINE 46 "templates\\GenericTemplate.hs" #-}
 
 
 
@@ -561,11 +628,21 @@ data NumList = NumListSingleton Int
 
 
 
-{-# LINE 67 "templates\\GenericTemplate.hs" #-}
+{-# LINE 43 "templates/GenericTemplate.hs" #-}
 
-{-# LINE 77 "templates\\GenericTemplate.hs" #-}
+data Happy_IntList = HappyCons Int Happy_IntList
 
-{-# LINE 86 "templates\\GenericTemplate.hs" #-}
+
+
+
+
+
+
+{-# LINE 65 "templates/GenericTemplate.hs" #-}
+
+{-# LINE 75 "templates/GenericTemplate.hs" #-}
+
+{-# LINE 84 "templates/GenericTemplate.hs" #-}
 
 infixr 9 `HappyStk`
 data HappyStk a = HappyStk a (HappyStk a)
@@ -589,7 +666,27 @@ happyAccept j tk st sts (HappyStk ans _) =
 -----------------------------------------------------------------------------
 -- Arrays only: do the next action
 
-{-# LINE 155 "templates\\GenericTemplate.hs" #-}
+{-# LINE 137 "templates/GenericTemplate.hs" #-}
+
+{-# LINE 147 "templates/GenericTemplate.hs" #-}
+indexShortOffAddr arr off = arr Happy_Data_Array.! off
+
+
+{-# INLINE happyLt #-}
+happyLt x y = (x < y)
+
+
+
+
+
+
+readArrayBit arr bit =
+    Bits.testBit (indexShortOffAddr arr (bit `div` 16)) (bit `mod` 16)
+
+
+
+
+
 
 -----------------------------------------------------------------------------
 -- HappyState data type (not arrays)
@@ -620,30 +717,30 @@ happyShift new_state i tk st sts stk =
 -- happyReduce is specialised for the common cases.
 
 happySpecReduce_0 i fn (1) tk st sts stk
-     = happyFail (1) tk st sts stk
+     = happyFail [] (1) tk st sts stk
 happySpecReduce_0 nt fn j tk st@((HappyState (action))) sts stk
      = action nt j tk st ((st):(sts)) (fn `HappyStk` stk)
 
 happySpecReduce_1 i fn (1) tk st sts stk
-     = happyFail (1) tk st sts stk
+     = happyFail [] (1) tk st sts stk
 happySpecReduce_1 nt fn j tk _ sts@(((st@(HappyState (action))):(_))) (v1`HappyStk`stk')
      = let r = fn v1 in
        happySeq r (action nt j tk st sts (r `HappyStk` stk'))
 
 happySpecReduce_2 i fn (1) tk st sts stk
-     = happyFail (1) tk st sts stk
+     = happyFail [] (1) tk st sts stk
 happySpecReduce_2 nt fn j tk _ ((_):(sts@(((st@(HappyState (action))):(_))))) (v1`HappyStk`v2`HappyStk`stk')
      = let r = fn v1 v2 in
        happySeq r (action nt j tk st sts (r `HappyStk` stk'))
 
 happySpecReduce_3 i fn (1) tk st sts stk
-     = happyFail (1) tk st sts stk
+     = happyFail [] (1) tk st sts stk
 happySpecReduce_3 nt fn j tk _ ((_):(((_):(sts@(((st@(HappyState (action))):(_))))))) (v1`HappyStk`v2`HappyStk`v3`HappyStk`stk')
      = let r = fn v1 v2 v3 in
        happySeq r (action nt j tk st sts (r `HappyStk` stk'))
 
 happyReduce k i fn (1) tk st sts stk
-     = happyFail (1) tk st sts stk
+     = happyFail [] (1) tk st sts stk
 happyReduce k nt fn j tk st sts stk
      = case happyDrop (k - ((1) :: Int)) sts of
          sts1@(((st1@(HappyState (action))):(_))) ->
@@ -651,7 +748,7 @@ happyReduce k nt fn j tk st sts stk
                 happyDoSeq r (action nt j tk st1 sts1 r)
 
 happyMonadReduce k nt fn (1) tk st sts stk
-     = happyFail (1) tk st sts stk
+     = happyFail [] (1) tk st sts stk
 happyMonadReduce k nt fn j tk st sts stk =
       case happyDrop k ((st):(sts)) of
         sts1@(((st1@(HappyState (action))):(_))) ->
@@ -659,7 +756,7 @@ happyMonadReduce k nt fn j tk st sts stk =
           happyThen1 (fn stk tk) (\r -> action nt j tk st1 sts1 (r `HappyStk` drop_stk))
 
 happyMonad2Reduce k nt fn (1) tk st sts stk
-     = happyFail (1) tk st sts stk
+     = happyFail [] (1) tk st sts stk
 happyMonad2Reduce k nt fn j tk st sts stk =
       case happyDrop k ((st):(sts)) of
         sts1@(((st1@(HappyState (action))):(_))) ->
@@ -669,6 +766,7 @@ happyMonad2Reduce k nt fn j tk st sts stk =
 
 
 
+             _ = nt :: Int
              new_state = action
 
           in
@@ -683,7 +781,7 @@ happyDropStk n (x `HappyStk` xs) = happyDropStk (n - ((1)::Int)) xs
 -----------------------------------------------------------------------------
 -- Moving to a new state after a reduction
 
-{-# LINE 256 "templates\\GenericTemplate.hs" #-}
+{-# LINE 267 "templates/GenericTemplate.hs" #-}
 happyGoto action j tk st = action j j tk (HappyState action)
 
 
@@ -691,10 +789,10 @@ happyGoto action j tk st = action j j tk (HappyState action)
 -- Error recovery ((1) is the error token)
 
 -- parse error if we are in recovery and we fail again
-happyFail (1) tk old_st _ stk@(x `HappyStk` _) =
+happyFail explist (1) tk old_st _ stk@(x `HappyStk` _) =
      let i = (case x of { HappyErrorToken (i) -> i }) in
 --      trace "failing" $ 
-        happyError_ i tk
+        happyError_ explist i tk
 
 {-  We don't need state discarding for our restricted implementation of
     "error".  In fact, it can cause some bogus parses, so I've disabled it
@@ -709,7 +807,7 @@ happyFail  (1) tk old_st (((HappyState (action))):(sts))
 
 -- Enter error recovery: generate an error token,
 --                       save the old token and carry on.
-happyFail  i tk (HappyState (action)) sts stk =
+happyFail explist i tk (HappyState (action)) sts stk =
 --      trace "entering error recovery" $
         action (1) (1) tk (HappyState (action)) sts ( (HappyErrorToken (i)) `HappyStk` stk)
 
@@ -742,7 +840,7 @@ happyDontSeq a b = b
 -- of deciding to inline happyGoto everywhere, which increases the size of
 -- the generated parser quite a bit.
 
-{-# LINE 322 "templates\\GenericTemplate.hs" #-}
+{-# LINE 333 "templates/GenericTemplate.hs" #-}
 {-# NOINLINE happyShift #-}
 {-# NOINLINE happySpecReduce_0 #-}
 {-# NOINLINE happySpecReduce_1 #-}
--- Grammar.y
+++ Grammar.y
@@ -25,56 +25,40 @@ import Tokens
 Prog : StrictVarList '{' ConstraintList '}'  {Query $1 $3}
 | StrictVarList '{' ConstraintList '}' Prog {ProgLink (Query $1 $3) $5}
 
-VarList : VarItem {VarListSingleton $1}
-| VarItem ',' VarList {VarListLink $1 $3}
+VarList : VarItem {[$1]}
+| VarItem ',' VarList {$1 : $3}
 
-StrictVarList : var {StrictVarListSingleton $1}
-| var ',' StrictVarList {StrictVarListLink $1 $3}
+StrictVarList : var {[$1]}
+| var ',' StrictVarList {$1 : $3}
 
 VarItem : var {VarItemVar $1}
 | '_' {VarItemBlank}
 
-ConstraintList : Constraint ';' {ConstraintListSingleton $1}
-| Constraint ';' ConstraintList {ConstraintListLink $1 $3}
+ConstraintList : Constraint ';' {[$1]}
+| Constraint ';' ConstraintList {$1 : $3}
 
 Constraint : rel '(' VarList ')' {ConstraintRel $1 $3}
 | rel '[' NumList ']' '(' VarList ')' {ConstraintRelEnhanced $1 $3 $6}
 | var '=' var {ConstraintEq $1 $3}
 
-NumList : num {NumListSingleton $1}
-| num ',' NumList {NumListLink $1 $3}
+NumList : num {[$1]}
+| num ',' NumList {$1 : $3}
 
 
 { 
 parseError :: [Token] -> a
 parseError _ = error "Parse error"
   
-data Prog = Query StrictVarList ConstraintList
+data Prog = Query [String] [Constraint]
          | ProgLink Prog Prog
          deriving Show
-
-data VarList = VarListSingleton VarItem
-  | VarListLink VarItem VarList
-  deriving Show
-  
-data StrictVarList = StrictVarListSingleton String
-  | StrictVarListLink String StrictVarList
-  deriving Show
 
 data VarItem = VarItemVar String
   | VarItemBlank
   deriving Show
 
-data ConstraintList = ConstraintListSingleton Constraint
-  | ConstraintListLink Constraint ConstraintList
-  deriving Show
-
-data Constraint = ConstraintRel String VarList
-  | ConstraintRelEnhanced String NumList VarList
+data Constraint = ConstraintRel String [VarItem]
+  | ConstraintRelEnhanced String [Int] [VarItem]
   | ConstraintEq String String
-  deriving Show
-
-data NumList = NumListSingleton Int
-  | NumListLink Int NumList
   deriving Show
 } 
