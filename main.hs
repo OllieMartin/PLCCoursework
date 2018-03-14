6210a9d347a383f738d768a689be163f490afe98
Billy Bradley <billy.jack.bradley@gmail.com>
2018-03-14 11:07:18 +0000

    added splitOn

--- main.hs
+++ main.hs
@@ -4,6 +4,6 @@ import Tokens
 import Grammar
 import Interpreter2
 
-main = do text <- readFile "program.txt"
+main = do text <- readFile "challenge.eql"
           result <- (eval (parseCalc (alexScanTokens text)))
           putStrLn result
